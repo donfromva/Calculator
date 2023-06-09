aa860707075f62265839c1d787930cb1f9851e32
Don <donfromva@gmail.com>
2023-06-09 00:57:34 -0400

    Fully functioning (Besides keyboard support)

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// FUNCTIONALITY
+// HOLDERS
 let firstNumber = ''
 let operator = ''
 let secondNumber = ''
@@ -17,6 +17,8 @@ const opEvaluate = document.getElementById('evaluate')
 const opDecimal = document.getElementById('decimal')
 const opClear = document.getElementById('clear')
 const opPosOrNeg = document.getElementById('pos-neg') // Needs work
+const opPercent = document.getElementById('percent')
+
 const opButtons = document.querySelectorAll('[data-operation]')
 opButtons.forEach(btn => btn.addEventListener('click', () => {
     operator = btn.textContent
@@ -24,19 +26,34 @@ opButtons.forEach(btn => btn.addEventListener('click', () => {
     currentOpDisplay.textContent = ''
 }))
 
-
-
-
-
 // EVENT LISTENERS
-opClear.addEventListener('click', () => {
-    clearOps()
+opClear.addEventListener('click', () => clearOps())
+opEvaluate.addEventListener('click', () => evaluate())
+opDecimal.addEventListener('click', () => appendDecimal())
+
+const percentSymbol = opPercent.textContent
+opPercent.addEventListener('click', () => {
+    if (currentOpDisplay.textContent == 0) return
+    reset = true
+    operation(percentSymbol)
+    return currentOpDisplay.textContent = percent(firstNumber)
 })
 
-opEvaluate.addEventListener('click', () => evaluate())
+opPosOrNeg.addEventListener('click', () => {
+    if (currentOpDisplay.textContent == 0) {
+        currentOpDisplay.textContent = '-'
+    } else if (!currentOpDisplay.textContent.includes('-')) {
+        currentOpDisplay.textContent = '-' + currentOpDisplay.textContent
+    } else if (currentOpDisplay.textContent.includes('-')) {
+        let displayArrayed = currentOpDisplay.textContent.split('')
+        let minusIndex = displayArrayed.indexOf('-')
+        delete displayArrayed[minusIndex]
+        currentOpDisplay.textContent = displayArrayed.join('')
+    }
+    if (currentOpDisplay.textContent == '') currentOpDisplay.textContent = 0
+})
 
-
-// FUNCTIONS
+// GENERAL FUNCTIONS
 const clearOps = () => {
     lastOpDisplay.textContent = ''
     currentOpDisplay.textContent = 0
@@ -44,46 +61,53 @@ const clearOps = () => {
 
 const appendNum = (num) => {
     if (currentOpDisplay.textContent.length === 20) return
-    if (lastOpDisplay.textContent.includes('=')) {
+    if (lastOpDisplay.textContent.includes('=') || lastOpDisplay.textContent.includes('%')) {
         lastOpDisplay.textContent = ''
     } 
-    if (currentOpDisplay.textContent == 0 || reset) {
+    if ((currentOpDisplay.textContent == 0 && currentOpDisplay.textContent.length === 1) || reset) {
         currentOpDisplay.textContent = ''
         reset = false
-    } 
+    }
 
     currentOpDisplay.textContent += num
 }
 
+const appendDecimal = () => {
+    if (!currentOpDisplay.textContent.includes('.')) {
+        currentOpDisplay.textContent += '.'
+    }
+}
+
 const operation = (operation) => {
     firstNumber = Number(currentOpDisplay.textContent)
+    if (currentOpDisplay.textContent == 0) {
+        alert('Please enter a number greater than or less than 0 before choosing an operation')
+        return
+    }
     lastOpDisplay.textContent = `${currentOpDisplay.textContent} ${operation}`
 }
 
 const evaluate = () => {
-    if (lastOpDisplay.textContent.length === 0 || lastOpDisplay.textContent.includes('=')) return
+    if (lastOpDisplay.textContent.length === 0 || lastOpDisplay.textContent.includes('=') || lastOpDisplay.textContent.includes('%')) return
     secondNumber = Number(currentOpDisplay.textContent)
     lastOpDisplay.textContent += ` ${secondNumber} =`
     reset = true
 
     switch(operator) {
-        case '%':
-            lastOpDisplay.textContent = `${firstNumber} ${operator}`
-            return currentOpDisplay.textContent = percent(firstNumber)
         case '÷':
-            return currentOpDisplay.textContent = divide(firstNumber, secondNumber)
+            return currentOpDisplay.textContent = Math.round((divide(firstNumber, secondNumber) + Number.EPSILON) * 100) / 100
         case '*':
-            return currentOpDisplay.textContent = multiply(firstNumber, secondNumber)
+            return currentOpDisplay.textContent = Math.round((multiply(firstNumber, secondNumber) + Number.EPSILON) * 100) / 100
         case '-':
-            return currentOpDisplay.textContent = subtract(firstNumber, secondNumber)
+            return currentOpDisplay.textContent = Math.round((subtract(firstNumber, secondNumber) + Number.EPSILON) * 100) / 100
         case '+':
-            return currentOpDisplay.textContent = add(firstNumber, secondNumber)
+            return currentOpDisplay.textContent = Math.round((add(firstNumber, secondNumber) + Number.EPSILON) * 100) / 100
         default: 
             break
     }
 }
 
-    // MATH 
+// MATH FUNCTIONS 
 function add(num1, num2) {
     return num1 + num2
 }
@@ -103,5 +127,3 @@ function divide(num1, num2) {
 function percent(num1) {
     return num1 * 0.01
 }
-
-
